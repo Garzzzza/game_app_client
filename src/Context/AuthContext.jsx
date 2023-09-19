84a87f4ea993d7ce014e5c4670f4621051fc0dfc
Amit <133666068+Garzzzza@users.noreply.github.com>
2023-09-19 15:21:31 +0300

    passValidationOnSignUp

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,53 +1,74 @@
 import { useState, createContext } from "react";
-import axios from "axios"
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [rePass, setRePass] = useState("");
+  const [nickName, setNickName] = useState("");
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [pass, setPass] = useState("");
-    const [rePass, setRePass] = useState("")
-    const [nickName, setNickName] = useState("")
+  const resetUserStates = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPass("");
+    setRePass("");
+    setNickName("");
+  };
 
-    const handleSignUp = async () => {
-        if (pass !== rePass) {
-            alert('Passwords do not match!');
-            return;
-        }
-
-        // Data to be sent to the backend
-        const userData = {
-            firstName: firstName,
-            lastName: lastName,
-            nickName: nickName,
-            email: email,
-            password: pass
-        };
-
-        try {
-            const response = await axios.post('/users', userData);
-
-
-        } catch (error) {
-            console.error('Error:', error);
-        }
+  const handleSignUp = async () => {
+    if (pass !== rePass) {
+      alert("Passwords do not match!");
+      return;
     }
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      nickName: nickName,
+      email: email,
+      password: pass,
+      rePassword: rePass,
+    };
 
+    try {
+      const response = await axios.post(
+        process.env.REACT_APP_SERVER_URL + "/users",
+        userData
+      );
+      resetUserStates();
+      Navigate("/");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-    return (
-        <AuthContext.Provider
-            value={{
-                firstName, setFirstName, lastName, setLastName, nickName, setNickName, email, setEmail, pass, setPass, rePass, setRePass, handleSignUp,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        firstName,
+        setFirstName,
+        lastName,
+        setLastName,
+        nickName,
+        setNickName,
+        email,
+        setEmail,
+        pass,
+        setPass,
+        rePass,
+        setRePass,
+        handleSignUp,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-
-export default AuthContextProvider
-export { AuthContext }
+export default AuthContextProvider;
+export { AuthContext };
