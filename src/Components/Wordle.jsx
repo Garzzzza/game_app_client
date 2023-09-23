a76f011e2835b922f37ccc805dcbc77ce68ffb18
Kellin Titenko <xkellinx@gmail.com>
2023-09-23 21:56:32 +0300

    first add on of kellins game

---
+++ src/Components/Wordle.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import UseWordle from '../hooks/UseWordel'
+
+// components
+import Grid from './Grid'
+import Keypad from './Keypad'
+
+export default function Wordle({ solution }) {
+  const { currentGuess, guesses, turn, isCorrect, handleKeyup } = UseWordle(solution)
+  
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyup)
+
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleKeyup])
+
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
+
+  return (
+    <div>
+      <div>solution - {solution}</div>
+      <div>Current Guess - {currentGuess}</div>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad />
+    </div>
+  )
+}
--- src/Pages/KGame.jsx
+++ src/Pages/KGame.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import UserScore from "../Components/UserScore";
 import { ScoreContext } from "../Context/ScoreContext";
+import Wordle from '../Components/Wordle';
 
 const KGame = () => {
   const { currentGame, setCurrentGame } = useContext(ScoreContext);
@@ -9,10 +10,51 @@ const KGame = () => {
     setCurrentGame("kgame");
   }, []);
 
+
+  // kellins code
+  const [solutions] = useState([
+    { word: "ninja", id: 1 },
+    { word: "apple", id: 2 },
+    { word: "banana", id: 3 },
+    { word: "cherry", id: 4 },
+    { word: "grape", id: 5 },
+  ]);
+
+  const [randomSolution, setRandomSolution] = useState(null);
+
+  useEffect(() => {
+  }, [randomSolution]);
+
+  const getRandomSolution = () => {
+    const randomIndex = Math.floor(Math.random() * solutions.length);
+    const randomWord = solutions[randomIndex].word;
+    const randomWordId = solutions[randomIndex].id;
+
+    // Update the randomSolution state in the App component
+    setRandomSolution({ word: randomWord, id: randomWordId });
+
+    // Log the random word and its ID to the console
+    console.log(`Random Word: ${randomWord}, ID: ${randomWordId}`);
+  };
+
   return (
     <div>
       <h1>KGame</h1>
       <UserScore />
+
+      {/* Kellins code */}
+      <h1>Wordle (Lingo)</h1>
+      {/* Display the random word and its ID */}
+      {randomSolution && (
+        <div>
+          <p>Random Word: {randomSolution.word}</p>
+          <p>Random Word ID: {randomSolution.id}</p>
+        </div>
+      )}
+
+      {randomSolution && <Wordle solution={randomSolution.word} />}
+      {/* Button to trigger getRandomSolution */}
+      <button onClick={getRandomSolution}>Get Random Word and ID</button>
     </div>
   );
 };
