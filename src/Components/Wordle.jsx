4735fcae56f0ed6f290a3c9bd53c0bc72aa8c4e5
Kellin Titenko <xkellinx@gmail.com>
2023-09-26 02:44:57 +0300

    the game works, the words are feched from the server, the game has score, but cant uploud to the server

--- src/Components/Wordle.jsx
+++ src/Components/Wordle.jsx
@@ -1,99 +1,40 @@
-// import React, { useEffect,  useState} from 'react'
-// import UseWordle from '../Components/UseWordle'
 
-// // components
-// import Grid from './Grid'
-// import Keypad from './Keypad'
-// import Modal from './Modal'
 
-// export default function Wordle({ solution }) {
-//   const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = UseWordle(solution)
-//   const [showModal, setShowModal] = useState(false)
-  
-//   useEffect(() => {
-//     window.addEventListener('keyup', handleKeyup)
-
-//     if (isCorrect) {
-//       setTimeout(() => setShowModal(true), 2000)
-//       window.removeEventListener('keyup', handleKeyup)
-//     }
-//     if (turn > 9) {
-//       setTimeout(() => setShowModal(true), 2000)
-//       window.removeEventListener('keyup', handleKeyup)
-//     }
-
-//     return () => window.removeEventListener('keyup', handleKeyup)
-//   }, [handleKeyup, isCorrect, turn])
-
-//   return (
-//     <div>
-//       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
-//       <Keypad usedKeys={usedKeys} />
-//       {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
-//     </div>
-//   )
-// }
-// import React, { useEffect, useState } from 'react';
-// import UseWordle from '../Components/UseWordle';
-// import Grid from './Grid';
-// import Keypad from './Keypad';
-// import Message from './Massage';
-
-// export default function Wordle({ solution }) {
-//   const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = UseWordle(solution);
-//   const [showMessage, setShowMessage] = useState(false);
-//   const score = isCorrect ? 1000 - 100 * (turn - 1) : 0;
-
-//   useEffect(() => {
-//     window.addEventListener('keyup', handleKeyup);
-
-//     if (isCorrect || turn > 9) {
-//         setShowMessage(true);
-//         window.removeEventListener('keyup', handleKeyup);
-//         // Call the handleShowButton function with true to show the button
-//         handleShowButton(true);
-//       }
-      
-
-//     return () => window.removeEventListener('keyup', handleKeyup);
-//   }, [handleKeyup, isCorrect, turn]);
-
-//   return (
-//     <div>
-//         {showMessage && (
-//         <Message isCorrect={isCorrect} turn={turn} solution={solution} score={score} />
-//       )}
-//       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
-//       <Keypad usedKeys={usedKeys} />
-//     </div>
-//   );
-// }
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import UseWordle from '../Components/UseWordle';
 import Grid from './Grid';
 import Keypad from './Keypad';
 import Message from './Massage';
+//import { ScoreContext } from '../Context/ScoreContext';
 
-export default function Wordle({ solution, showButton, handleShowButton, handleSolution }) {
+export default function Wordle({ solution, showButton, handleShowButton, handleSolution, description }) {
   const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = UseWordle(solution);
   const [showMessage, setShowMessage] = useState(false);
   const score = isCorrect ? 1000 - 100 * (turn - 1) : 0;
+  //const { postScore, currentGame, setCurrentScore } = useContext(ScoreContext);
 
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup);
 
     if (isCorrect || turn > 9) {
-      setShowMessage(true);
-      window.removeEventListener('keyup', handleKeyup);
-      // add the score to the db!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-      // Call the handleShowButton function to set showButton to true in KGame
-      //handleShowButton(true);
-
+        setShowMessage(true);
+        window.removeEventListener('keyup', handleKeyup);
+    
+        // Call the postScore function to add the score to the database
+        // if (isCorrect) {
+        //   postScore(currentGame);
+          
+        //   // Update the current score in the context if isCorrect is true
+        //   setCurrentScore(score);
+          
+        //   // Log the score to the console
+        //   console.log(`Current Score: ${score}`);
+        // }
+      
       // Delay the execution of handleSolution by 7 seconds
       setTimeout(() => {
         handleSolution();
+        handleShowButton(true);
       }, 7000);
 
     }
--- src/Pages/KGame.jsx
+++ src/Pages/KGame.jsx
@@ -1,21 +1,118 @@
 
-import axios from "axios"; // Import Axios
+// import axios from "axios"; // Import Axios
+// import React, { useContext, useEffect, useState } from "react";
+// import UserScore from "../Components/UserScore";
+// import { ScoreContext } from "../Context/ScoreContext";
+// import Wordle from '../Components/Wordle';
+// import { AuthContext } from "../Context/AuthContext";
+
+// const KGame = () => {
+//   const { currentGame, setCurrentGame } = useContext(ScoreContext);
+
+  // const [showButton, setShowButton] = useState(true); 
+
+//   const [randomSolution, setRandomSolution] = useState(null);
+
+//   useEffect(() => {
+//     setCurrentGame("kgame");
+//   }, []);
+
+  // const handleShowButton = (value) => {
+  //   setShowButton(value);
+  // };
+
+  // const handleSolution = () => {
+  //   setRandomSolution(null);
+  // };
+  
+//   const [solutions] = useState([
+//     { word: "Whisk", id: 1, description: "This kitchen tool sounds like a fast cat's tail." },
+//     { word: "Giddy", id: 2, description: "The feeling you get on a rollercoaster or when you win a prize." },
+//     { word: "Plump", id: 3, description: "It's what happens to your cheeks when you eat too many cookies." },
+//     { word: "Jelly", id: 4, description: "Peanut butter's best friend in a sandwich." },
+//     { word: "Beach", id: 5, description: "Where the sand meets the waves, and the sun loves to shine." },
+//     { word: "Magic", id: 6, description: "It makes rabbits disappear and brings smiles to faces." },
+//     { word: "Robot", id: 7, description: "A mechanical friend that can do some cool tricks." },
+//     { word: "Dance", id: 8, description: "What you do when the music takes control of your feet." },
+//     { word: "Tasty", id: 9, description: "Food that makes your taste buds throw a party." },
+//     { word: "Smile", id: 10, description: "What happens when you're happy or trying to make someone else happy." },
+// ]);
+
+
+// const { token } = useContext(AuthContext);
+
+// const [solutions_1, setSolutions] = useState([]);
+
+// const getSolutionsArray = async () => {
+//   try {
+//     const response = await axios.get(
+//       process.env.REACT_APP_SERVER_URL + "/kgame",
+//       { headers: { Authorization: "Bearer " + token } }
+//     );
+//     setSolutions(response.data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+//   useEffect(() => {
+//   }, [randomSolution]);
+
+//   const getRandomSolution = () => {
+//     const randomIndex = Math.floor(Math.random() * solutions.length);
+//     const randomWord = solutions[randomIndex].word;
+//     const randomWordId = solutions[randomIndex].id;
+//     const randomWordDis = solutions[randomIndex].description;
+
+//     // Update the randomSolution state in the App component
+//     setRandomSolution({ word: randomWord, id: randomWordId, description: randomWordDis });
+
+//     // Log the random word and its ID to the console
+//     console.log(`Random Word: ${randomWord}, ID: ${randomWordId}`);
+
+//     // Set showButton to true when message is shown
+//     handleShowButton(false);
+
+//     console.log(getSolutionsArray ());
+//   };
+
+//   return (
+//     <div>
+//       <h1>WordleWonder</h1>
+//       <UserScore />
+
+//       {/* Display the random word and its ID */}
+//       {randomSolution && (
+//         <div>
+//           <p>Description: {randomSolution.description}</p>
+//         </div>
+//       )}
+
+//       {randomSolution && <Wordle solution={randomSolution.word} handleShowButton={handleShowButton}  handleSolution={handleSolution} />}
+//       {/* Button to trigger getRandomSolution */}
+//       {showButton && <button onClick={getRandomSolution}>Let's play!</button>}
+//     </div>
+//   );
+// };
+
+// export default KGame;
+
+
 import React, { useContext, useEffect, useState } from "react";
 import UserScore from "../Components/UserScore";
 import { ScoreContext } from "../Context/ScoreContext";
-import Wordle from '../Components/Wordle';
+import Wordle from "../Components/Wordle";
+import { AuthContext } from "../Context/AuthContext";
+import axios from "axios";
+
 
 const KGame = () => {
-  const { currentGame, setCurrentGame } = useContext(ScoreContext);
+  //const { currentGame, setCurrentGame } = useContext(ScoreContext);
+  const { token } = useContext(AuthContext);
 
+  const [solutions, setSolutions] = useState([]);
   const [showButton, setShowButton] = useState(true); 
-
-  const [randomSolution, setRandomSolution] = useState(null);
-
-  useEffect(() => {
-    setCurrentGame("kgame");
-  }, []);
-
+  
   const handleShowButton = (value) => {
     setShowButton(value);
   };
@@ -23,41 +120,56 @@ const KGame = () => {
   const handleSolution = () => {
     setRandomSolution(null);
   };
-  
-  const [solutions] = useState([
-    { word: "Whisk", id: 1, description: "This kitchen tool sounds like a fast cat's tail." },
-    { word: "Giddy", id: 2, description: "The feeling you get on a rollercoaster or when you win a prize." },
-    { word: "Plump", id: 3, description: "It's what happens to your cheeks when you eat too many cookies." },
-    { word: "Jelly", id: 4, description: "Peanut butter's best friend in a sandwich." },
-    { word: "Beach", id: 5, description: "Where the sand meets the waves, and the sun loves to shine." },
-    { word: "Magic", id: 6, description: "It makes rabbits disappear and brings smiles to faces." },
-    { word: "Robot", id: 7, description: "A mechanical friend that can do some cool tricks." },
-    { word: "Dance", id: 8, description: "What you do when the music takes control of your feet." },
-    { word: "Tasty", id: 9, description: "Food that makes your taste buds throw a party." },
-    { word: "Smile", id: 10, description: "What happens when you're happy or trying to make someone else happy." },
-]);
 
- 
+  const getSolutionsArray = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_SERVER_URL + "/kgame",
+        { headers: { Authorization: "Bearer " + token } }
+      );
+      setSolutions(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-  }, [randomSolution]);
+    //setCurrentGame("kgame");
+    getSolutionsArray();
+  }, []);
 
+  const [randomSolution, setRandomSolution] = useState(null);
+
+  useEffect(() => {}, [randomSolution]);
+
+  // const getRandomSolution = () => {
+  //   const randomIndex = Math.floor(Math.random() * solutions.length);
+  //   const randomWord = solutions[randomIndex].word;
+  //   const randomWordId = solutions[randomIndex]._id;
+
+  //   // Update the randomSolution state in the App component
+  //   setRandomSolution({ word: randomWord, _id: randomWordId });
+
+  //   // Log the random word and its ID to the console
+  //   console.log(`Random Word: ${randomWord}, ID: ${randomWordId}`);
+  //   handleShowButton(false);
+  // };
   const getRandomSolution = () => {
     const randomIndex = Math.floor(Math.random() * solutions.length);
-    const randomWord = solutions[randomIndex].word;
-    const randomWordId = solutions[randomIndex].id;
+    const randomWord = solutions[randomIndex].word.toLowerCase(); // Convert to lowercase
+    const randomWordId = solutions[randomIndex]._id;
     const randomWordDis = solutions[randomIndex].description;
-
+  
     // Update the randomSolution state in the App component
-    setRandomSolution({ word: randomWord, id: randomWordId, description: randomWordDis });
-
+    setRandomSolution({ word: randomWord, _id: randomWordId, description:randomWordDis });
+  
     // Log the random word and its ID to the console
     console.log(`Random Word: ${randomWord}, ID: ${randomWordId}`);
-
-    // Set showButton to true when message is shown
     handleShowButton(false);
   };
 
+  
   return (
     <div>
       <h1>WordleWonder</h1>
@@ -70,7 +182,7 @@ const KGame = () => {
         </div>
       )}
 
-      {randomSolution && <Wordle solution={randomSolution.word} handleShowButton={handleShowButton}  handleSolution={handleSolution} />}
+      {randomSolution && <Wordle solution={randomSolution.word.toLowerCase()} handleShowButton={handleShowButton}  handleSolution={handleSolution} description={randomSolution.description} />}
       {/* Button to trigger getRandomSolution */}
       {showButton && <button onClick={getRandomSolution}>Let's play!</button>}
     </div>
@@ -78,5 +190,3 @@ const KGame = () => {
 };
 
 export default KGame;
-
-
